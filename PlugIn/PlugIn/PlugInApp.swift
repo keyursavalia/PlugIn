13b0649db3dfb07d706a5deb349cdd13c22c0d72
Keyur <savalia.keyur1091@gmail.com>
2026-02-17 23:31:38 -0800

    chore: add Xcode project scaffold and SPM dependency lock file

---
+++ PlugIn/PlugIn/PlugInApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlugInApp.swift
+//  PlugIn
+//
+//  Created by Keyur Savalia on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct PlugInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
